f2f536575d686fed65b5d538d90dfbdf421f16f2
idonans <idonans@126.com>
2021-03-10 10:18:34 +0800

    improve Sequence SignGenerator impl

--- imsdk/src/main/java/com/masonsoft/imsdk/db/Sequence.java
+++ imsdk/src/main/java/com/masonsoft/imsdk/db/Sequence.java
@@ -3,18 +3,25 @@ package com.masonsoft.imsdk.db;
 /**
  * 生成数据库序列值(在不同的表或者不同的会话中，序列值可能会重复).<br/>
  * 序列号最大 63 位(有符号长整型)<br/>
- * 其中：低 55 位为时间戳位，高 8 位为功能位<br/>
  * <table>
  *   <tr>
- *     <th>第几位</td>
+ *     <th>第几位(由低到高)</td>
  *     <th>含义</td>
  *   </tr>
  *   <tr>
- *     <td>第1-55位(低55位)</td>
+ *     <td>第1-4位(低4位)</td>
+ *     <td>循环序列号(0-0xF)</td>
+ *   </tr>
+ *   <tr>
+ *     <td>第5位</td>
+ *     <td>预留功能位</td>
+ *   </tr>
+ *   <tr>
+ *     <td>第6-60位(共55位)</td>
  *     <td>时间戳(可表示至3021年的微秒)</td>
  *   </tr>
  *   <tr>
- *     <td>第56-61位</td>
+ *     <td>第61位</td>
  *     <td>预留功能位</td>
  *   </tr>
  *   <tr>
@@ -32,8 +39,13 @@ public class Sequence {
     /**
      * 置顶标记
      */
-    private static final long TOP_FLAG = 0x01L << 61;
-    private static final long TIME_MAX = Long.MAX_VALUE >> 8;
+    private static final long TOP_FLAG = 0x1L << 61;
+    @SuppressWarnings("NumericOverflow")
+    private static final long TIME_MASK = (Long.MAX_VALUE >> 3) & (Long.MAX_VALUE << 5);
+    private static final long INDEX_MASK = 0xFL;
+
+    private static int sIndex;
+    private static final Object INDEX_LOCK = new Object();
 
     /**
      * 创建一个序列值
@@ -42,11 +54,19 @@ public class Sequence {
      * @param timeMicroSeconds 时间戳微秒
      */
     public static long create(boolean top, long timeMicroSeconds) {
-        long sequence = TIME_MAX & timeMicroSeconds;
+        long sequence = TIME_MASK & (timeMicroSeconds << 5);
         if (top) {
             sequence += TOP_FLAG;
         }
+        sequence += nextIndex() & INDEX_MASK;
         return sequence;
+    }
+
+    private static int nextIndex() {
+        synchronized (INDEX_LOCK) {
+            sIndex = (sIndex++) % 0x10;
+            return sIndex;
+        }
     }
 
 }
