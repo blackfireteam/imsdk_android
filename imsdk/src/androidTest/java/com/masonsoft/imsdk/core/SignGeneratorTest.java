3ff6701eeb7eff4974a81ebf7e790f7cc6be81ab
idonans <idonans@126.com>
2021-03-11 13:58:46 +0800

    add SignGeneratorTest

---
+++ imsdk/src/androidTest/java/com/masonsoft/imsdk/core/SignGeneratorTest.java
@@ -0,0 +1,34 @@
+package com.masonsoft.imsdk.core;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.concurrent.TimeUnit;
+
+@RunWith(AndroidJUnit4.class)
+public class SignGeneratorTest {
+
+    @Test
+    public void testGeneralSpeed() {
+        // 期望每毫秒生成的 sign 数量
+        final long speed = 1000;
+        final long count = 99999;
+
+        final long timeStart = System.nanoTime();
+        for (int i = 0; i < count; i++) {
+            SignGenerator.next();
+        }
+        final long timeEnd = System.nanoTime();
+        final long timeConsumed = timeEnd - timeStart;
+        final double timeConsumedMs = TimeUnit.NANOSECONDS.toMillis(timeConsumed);
+
+        // 实际测试结果每毫秒生成的 sign 数量
+        final long speedTest = (long) (count * 1f / timeConsumedMs);
+
+        Assert.assertTrue("Sign 生成速度(个/毫秒)太慢，期望值：" + speed + ", 实际值：" + speedTest, speedTest >= speed);
+    }
+
+}
