cb4cdcb99f9fc619258b8d1328b91bfd3ab821a5
idonans <idonans@126.com>
2021-03-15 17:25:04 +0800

    add SequenceTest

---
+++ imsdk/src/androidTest/java/com/masonsoft/imsdk/db/SequenceTest.java
@@ -0,0 +1,24 @@
+package com.masonsoft.imsdk.db;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.masonsoft.imsdk.core.SignGenerator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class SequenceTest {
+
+    @Test
+    public void testSequence() {
+        for (int i = 0; i < 10; i++) {
+            final long sign = SignGenerator.next();
+            final long seq = Sequence.create(sign);
+            System.out.println("===========================================");
+            System.out.println("sign:" + Long.toBinaryString(sign));
+            System.out.println("seq:" + Long.toBinaryString(seq));
+        }
+    }
+
+}
