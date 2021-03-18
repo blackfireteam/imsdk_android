7aafc60f483c21cebab36be1e0c97b97297ff8e3
idonans <idonans@126.com>
2021-03-18 10:26:05 +0800

    add I18nResources

---
+++ imsdk/src/main/java/com/masonsoft/imsdk/core/I18nResources.java
@@ -0,0 +1,17 @@
+package com.masonsoft.imsdk.core;
+
+import androidx.annotation.StringRes;
+
+import com.idonans.core.util.ContextUtil;
+
+public final class I18nResources {
+
+    public static String getString(@StringRes int resId) {
+        return ContextUtil.getContext().getString(resId);
+    }
+
+    public static String getString(@StringRes int resId, Object... formatArgs) {
+        return ContextUtil.getContext().getString(resId, formatArgs);
+    }
+
+}
--- imsdk/src/main/java/com/masonsoft/imsdk/core/processor/SendMessageSessionValidateProcessor.java
+++ imsdk/src/main/java/com/masonsoft/imsdk/core/processor/SendMessageSessionValidateProcessor.java
@@ -3,6 +3,8 @@ package com.masonsoft.imsdk.core.processor;
 import androidx.annotation.Nullable;
 
 import com.masonsoft.imsdk.IMSessionMessage;
+import com.masonsoft.imsdk.R;
+import com.masonsoft.imsdk.core.I18nResources;
 import com.masonsoft.imsdk.core.IMLog;
 import com.masonsoft.imsdk.core.RuntimeMode;
 import com.masonsoft.imsdk.lang.Processor;
@@ -20,7 +22,12 @@ public class SendMessageSessionValidateProcessor implements Processor<IMSessionM
         }
 
         if (target.getSessionUserId() <= 0) {
-            target.getEnqueueCallback().onEnqueueFail(target, IMSessionMessage.EnqueueCallback.ERROR_CODE_INVALID_SESSION_USER_ID, "未登录");
+            target.getEnqueueCallback().onEnqueueFail(
+                    target,
+                    IMSessionMessage.EnqueueCallback.ERROR_CODE_INVALID_SESSION_USER_ID,
+                    I18nResources.getString(R.string.msimsdk_enqueue_callback_error_invalid_session_user_id)
+            );
+
             return true;
         }
         return false;
