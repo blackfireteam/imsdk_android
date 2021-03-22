a4be349e1681715c3416bcac8b65231f45625399
idonans <idonans@126.com>
2021-03-22 11:11:37 +0800

    update splash theme

--- app/src/main/java/com/masonsoft/imsdk/sample/MainActivity.java
+++ app/src/main/java/com/masonsoft/imsdk/sample/MainActivity.java
@@ -1,14 +1,31 @@
 package com.masonsoft.imsdk.sample;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.idonans.systeminsets.SystemUiHelper;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static void start(Context context) {
+        Intent starter = new Intent(context, MainActivity.class);
+        context.startActivity(starter);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        SystemUiHelper.from(getWindow())
+                .layoutStatusBar()
+                .layoutStable()
+                .setLightStatusBar()
+                .setLightNavigationBar()
+                .apply();
+
         setContentView(R.layout.imsdk_sample_main_activity);
     }
 
--- app/src/main/java/com/masonsoft/imsdk/sample/SplashActivity.java
+++ app/src/main/java/com/masonsoft/imsdk/sample/SplashActivity.java
@@ -5,18 +5,98 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.idonans.core.WeakAbortSignal;
+import com.idonans.core.thread.Threads;
+import com.idonans.systeminsets.SystemUiHelper;
 import com.masonsoft.imsdk.sample.databinding.ImsdkSampleSplashActivityBinding;
 
 public class SplashActivity extends AppCompatActivity {
 
-    private ImsdkSampleSplashActivityBinding mBinding;
+    private boolean mPendingRedirect;
+    private boolean mStarted;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        mBinding = ImsdkSampleSplashActivityBinding.inflate(getLayoutInflater());
-        setContentView(mBinding.getRoot());
+        SystemUiHelper.from(getWindow())
+                .layoutStatusBar()
+                .layoutStable()
+                .setLightStatusBar()
+                .setLightNavigationBar()
+                .apply();
+
+        final ImsdkSampleSplashActivityBinding binding = ImsdkSampleSplashActivityBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.imsdkNameText.setText(com.masonsoft.imsdk.BuildConfig.LIB_NAME);
+        binding.imsdkVersionText.setText(buildImsdkVersionText());
+
+        Threads.postUi(new RedirectToMain(this), 1500L);
+    }
+
+    private static String buildImsdkVersionText() {
+        return com.masonsoft.imsdk.BuildConfig.LIB_VERSION_NAME + "(" + com.masonsoft.imsdk.BuildConfig.LIB_VERSION_CODE + ")";
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mStarted = true;
+
+        if (mPendingRedirect) {
+            doRedirect();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mStarted = false;
+    }
+
+    private void dispatchRedirect() {
+        if (isFinishing()) {
+            return;
+        }
+
+        if (!mStarted) {
+            mPendingRedirect = true;
+            return;
+        }
+
+        doRedirect();
+    }
+
+    private void doRedirect() {
+        if (isFinishing()) {
+            return;
+        }
+
+        MainActivity.start(this);
+        finish();
+    }
+
+    private static class RedirectToMain extends WeakAbortSignal implements Runnable {
+
+        public RedirectToMain(@Nullable SplashActivity splashActivity) {
+            super(splashActivity);
+        }
+
+        @Override
+        public void run() {
+            if (isAbort()) {
+                return;
+            }
+
+            SplashActivity splashActivity = (SplashActivity) getObject();
+            if (splashActivity == null) {
+                return;
+            }
+
+            splashActivity.dispatchRedirect();
+        }
+
     }
 
 }
