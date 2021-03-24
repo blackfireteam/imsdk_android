edc6f136926aaf48304ea3ce8787ede069511f8c
idonans <idonans@126.com>
2021-03-24 18:38:39 +0800

    fix emoji init

--- app/src/main/java/com/masonsoft/imsdk/sample/SampleApplication.java
+++ app/src/main/java/com/masonsoft/imsdk/sample/SampleApplication.java
@@ -3,6 +3,9 @@ package com.masonsoft.imsdk.sample;
 import android.app.Application;
 import android.util.Log;
 
+import androidx.emoji.bundled.BundledEmojiCompatConfig;
+import androidx.emoji.text.EmojiCompat;
+
 import com.masonsoft.imsdk.core.IMLog;
 
 public class SampleApplication extends Application {
@@ -13,6 +16,8 @@ public class SampleApplication extends Application {
 
         IMLog.setLogLevel(Log.VERBOSE);
         SampleLog.setLogLevel(Log.VERBOSE);
+
+        EmojiCompat.init(new BundledEmojiCompatConfig(this));
     }
 
 }
