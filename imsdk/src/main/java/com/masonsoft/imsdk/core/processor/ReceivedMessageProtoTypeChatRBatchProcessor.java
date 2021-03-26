e6f6cfbd4bcb683d48aa0a0d36d10725ce7fc9c0
idonans <idonans@126.com>
2021-03-26 18:10:14 +0800

    improve ReceivedMessageProtoTypeChatRBatchProcessor

--- imsdk/src/main/java/com/masonsoft/imsdk/core/processor/ReceivedMessageProtoTypeChatRBatchProcessor.java
+++ imsdk/src/main/java/com/masonsoft/imsdk/core/processor/ReceivedMessageProtoTypeChatRBatchProcessor.java
@@ -1,8 +1,17 @@
 package com.masonsoft.imsdk.core.processor;
 
+import android.database.sqlite.SQLiteDatabase;
+
 import androidx.annotation.NonNull;
 
+import com.masonsoft.imsdk.core.IMConstants;
+import com.masonsoft.imsdk.core.IMLog;
+import com.masonsoft.imsdk.core.block.MessageBlock;
+import com.masonsoft.imsdk.core.db.DatabaseHelper;
+import com.masonsoft.imsdk.core.db.DatabaseProvider;
+import com.masonsoft.imsdk.core.db.DatabaseSessionWriteLock;
 import com.masonsoft.imsdk.core.db.Message;
+import com.masonsoft.imsdk.core.db.MessageDatabaseProvider;
 import com.masonsoft.imsdk.core.db.MessageFactory;
 import com.masonsoft.imsdk.core.message.SessionProtoByteMessageWrapper;
 import com.masonsoft.imsdk.core.proto.ProtoMessage;
@@ -36,13 +45,130 @@ public class ReceivedMessageProtoTypeChatRBatchProcessor extends ReceivedMessage
             }
         }
 
-        // messageList 中的所有消息是连续的，并且是有序的(按照 msg id 有序)
-        if (!messageList.isEmpty()) {
-            // TODO 交给消息发送队列处理
+        if (messageList.isEmpty()) {
+            IMLog.e(new IllegalArgumentException("unexpected ChatRBatch is empty"));
+            return true;
         }
 
-        // TODO
-        return false;
+        // messageList 中的所有消息是连续的，并且是有序的(按照 msg id 有序)
+
+        // server message id 最大和最小的那一条(对比第一条和最后一条)
+        final Message minMessage, maxMessage;
+        {
+            final Message firstMessage = messageList.get(0);
+            final Message lastMessage = messageList.get(messageList.size() - 1);
+            if (firstMessage.remoteMessageId.get() < lastMessage.remoteMessageId.get()) {
+                minMessage = firstMessage;
+                maxMessage = lastMessage;
+            } else {
+                minMessage = lastMessage;
+                maxMessage = firstMessage;
+            }
+        }
+
+        final long sessionUserId = target.getSessionUserId();
+        final long fromUserId = maxMessage.fromUserId.get();
+        final long toUserId = maxMessage.toUserId.get();
+        if (fromUserId != sessionUserId && toUserId != sessionUserId) {
+            IMLog.e("unexpected. sessionUserId:%s invalid fromUserId and toUserId %s", sessionUserId, maxMessage);
+            return false;
+        }
+
+        final boolean received = fromUserId != sessionUserId;
+        final long targetUserId = received ? fromUserId : toUserId;
+        final int conversationType = IMConstants.ConversationType.C2C;
+
+        for (Message message : messageList) {
+            message.applyLogicField(sessionUserId, conversationType, targetUserId);
+
+            final int messageType = message.messageType.get();
+            if (messageType == IMConstants.MessageType.REVOKE_MESSAGE) {
+                // 撤回了一条消息，如果目标消息在本地，则需要将目标消息的类型修改为已撤回
+                final long targetMessageId = Long.parseLong(message.body.get().trim());
+                final Message dbMessage = MessageDatabaseProvider.getInstance().getMessageWithRemoteMessageId(
+                        sessionUserId,
+                        conversationType,
+                        targetUserId,
+                        targetMessageId);
+                if (dbMessage != null) {
+                    if (dbMessage.messageType.get() != IMConstants.MessageType.REVOKED) {
+                        // 将本地目标消息修改为已撤回
+                        final DatabaseHelper databaseHelper = DatabaseProvider.getInstance().getDBHelper(sessionUserId);
+                        synchronized (DatabaseSessionWriteLock.getInstance().getSessionWriteLock(databaseHelper)) {
+                            final Message messageUpdate = new Message();
+                            messageUpdate.localId.apply(dbMessage.localId);
+                            messageUpdate.messageType.set(IMConstants.MessageType.REVOKED);
+                            MessageDatabaseProvider.getInstance().updateMessage(
+                                    sessionUserId,
+                                    conversationType,
+                                    targetUserId,
+                                    messageUpdate);
+                        }
+                    }
+                }
+            }
+        }
+
+        final long blockId = MessageBlock.generateBlockId(
+                sessionUserId,
+                conversationType,
+                targetUserId,
+                maxMessage.remoteMessageId.get()
+        );
+        final DatabaseHelper databaseHelper = DatabaseProvider.getInstance().getDBHelper(sessionUserId);
+        synchronized (DatabaseSessionWriteLock.getInstance().getSessionWriteLock(databaseHelper)) {
+            final SQLiteDatabase database = databaseHelper.getDBHelper().getWritableDatabase();
+            database.beginTransaction();
+            try {
+                for (Message message : messageList) {
+                    final long remoteMessageId = message.remoteMessageId.get();
+                    // 将发送状态设置为发送成功
+                    message.localSendStatus.set(IMConstants.SendStatus.SUCCESS);
+                    // 设置 block id
+                    message.localBlockId.set(blockId);
+
+                    final Message dbMessage = MessageDatabaseProvider.getInstance().getMessageWithRemoteMessageId(
+                            sessionUserId,
+                            conversationType,
+                            targetUserId,
+                            remoteMessageId);
+                    if (dbMessage == null) {
+                        // 如果消息在本地不存在，则入库
+                        if (!MessageDatabaseProvider.getInstance().insertMessage(
+                                sessionUserId,
+                                conversationType,
+                                targetUserId,
+                                message)) {
+                            final Throwable e = new IllegalAccessError("unexpected insertMessage return false " + message);
+                            IMLog.e(e);
+                        }
+                    } else {
+                        // 消息已经存在，更新必要字段
+                        final Message updateMessage = new Message();
+                        updateMessage.applyLogicField(sessionUserId, conversationType, targetUserId);
+                        // 更新发送状态为发送成功
+                        updateMessage.localSendStatus.set(IMConstants.SendStatus.SUCCESS);
+                        // 更新 block id
+                        updateMessage.localBlockId.set(blockId);
+                        updateMessage.localId.set(dbMessage.localId.get());
+                        if (!MessageDatabaseProvider.getInstance().updateMessage(
+                                sessionUserId, conversationType, targetUserId, updateMessage)) {
+                            final Throwable e = new IllegalAccessError("unexpected updateMessage return false " + updateMessage);
+                            IMLog.e(e);
+                        }
+                    }
+                }
+
+                // expand block id
+                MessageBlock.expandBlockId(sessionUserId, conversationType, targetUserId, minMessage.remoteMessageId.get());
+
+                database.setTransactionSuccessful();
+            } finally {
+                database.endTransaction();
+            }
+        }
+
+        return true;
     }
 
 }
