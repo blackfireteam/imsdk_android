6977f646a188e4266a09515d1dc5526054b853b2
idonans <idonans@126.com>
2021-03-30 17:25:02 +0800

    improve IMMessageUploadManager

--- imsdk/src/main/java/com/masonsoft/imsdk/core/IMMessageUploadManager.java
+++ imsdk/src/main/java/com/masonsoft/imsdk/core/IMMessageUploadManager.java
@@ -4,16 +4,20 @@ import android.graphics.Bitmap;
 import android.text.TextUtils;
 
 import androidx.annotation.NonNull;
-import androidx.collection.LongSparseArray;
+import androidx.annotation.Nullable;
 
 import com.idonans.core.Singleton;
 import com.idonans.core.thread.TaskQueue;
 import com.masonsoft.imsdk.core.db.LocalSendingMessage;
 import com.masonsoft.imsdk.core.db.LocalSendingMessageProvider;
+import com.masonsoft.imsdk.core.db.Message;
+import com.masonsoft.imsdk.core.db.MessageDatabaseProvider;
 import com.masonsoft.imsdk.lang.SafetyRunnable;
+import com.masonsoft.imsdk.util.Objects;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -67,18 +71,195 @@ public class IMMessageUploadManager {
         getSessionUploader(sessionUserId).dispatchCheckIdleMessage();
     }
 
+    private static class MessageUploadObjectWrapper {
+        //////////////////////////////////////////////////////////////////////
+        //////////////////////////////////////////////////////////////////////
+        //////////////////////////////////////////////////////////////////////
+        private static final int FIRST_LOCAL_ERROR_CODE = Integer.MIN_VALUE / 2;
+        /**
+         * 未知错误
+         */
+        private static final int ERROR_CODE_UNKNOWN = FIRST_LOCAL_ERROR_CODE;
+        private static final int ERROR_CODE_TARGET_NOT_FOUND = FIRST_LOCAL_ERROR_CODE + 1;
+        /**
+         * 绑定 abort id 失败
+         */
+        private static final int ERROR_CODE_BIND_ABORT_ID_FAIL = FIRST_LOCAL_ERROR_CODE + 2;
+        /**
+         * 错误的 send status 状态
+         */
+        private static final int ERROR_CODE_UNEXPECTED_SEND_STATUS = FIRST_LOCAL_ERROR_CODE + 3;
+        /**
+         * 更新 sendStatus 失败
+         */
+        private static final int ERROR_CODE_UPDATE_SEND_STATUS_FAIL = FIRST_LOCAL_ERROR_CODE + 4;
+
+        //////////////////////////////////////////////////////////////////////
+        //////////////////////////////////////////////////////////////////////
+        //////////////////////////////////////////////////////////////////////
+        private final long mSessionUserId;
+        private final long mSign;
+        private final long mAbortId;
+        @NonNull
+        private LocalSendingMessage mLocalSendingMessage;
+        @Nullable
+        private Message mMessage;
+
+        /**
+         * abort id 与数据库中对应记录不相等.
+         */
+        private boolean mAbortIdNotMatch;
+
+        /**
+         * 消息的发送进度
+         */
+        public float mSendProgress = 0f;
+
+        public int mErrorCode;
+        public String mErrorMessage;
+
+        //////////////////////////////////////////////////////////////////////
+        //////////////////////////////////////////////////////////////////////
+        //////////////////////////////////////////////////////////////////////
+
+        public MessageUploadObjectWrapper(long sessionUserId, long sign, long abortId, @NonNull LocalSendingMessage localSendingMessage) {
+            this.mSessionUserId = sessionUserId;
+            this.mSign = sign;
+            this.mAbortId = abortId;
+            this.mLocalSendingMessage = localSendingMessage;
+        }
+
+        private void setError(int errorCode, String errorMessage) {
+            this.mErrorCode = errorCode;
+            this.mErrorMessage = errorMessage;
+        }
+
+        private boolean isFastMessage() {
+            final Message message = mMessage;
+            if (message == null) {
+                return false;
+            }
+            if (message.messageType.isUnset()) {
+                return false;
+            }
+            final int messageType = message.messageType.get();
+            if (messageType == IMConstants.MessageType.TEXT) {
+                return true;
+            }
+            return false;
+        }
+
+        /**
+         * 上传任务执行结束。如果任务执行成功，则从上传表中的删除。否则设置为上传失败。
+         */
+        private void onTaskEnd() {
+            final LocalSendingMessage localSendingMessage = mLocalSendingMessage;
+            if (localSendingMessage.localSendStatus.get() != IMConstants.SendStatus.SUCCESS) {
+                moveSendStatus(IMConstants.SendStatus.FAIL);
+            } else {
+                LocalSendingMessageProvider.getInstance().removeLocalSendingMessage(
+                        mSessionUserId,
+                        localSendingMessage.localId.get());
+            }
+        }
+
+        /**
+         * 绑定消息上传任务的 abort id
+         */
+        private void bindAbortId() {
+            try {
+                final LocalSendingMessage localSendingMessageUpdate = new LocalSendingMessage();
+                localSendingMessageUpdate.localId.set(this.mLocalSendingMessage.localId.get());
+                localSendingMessageUpdate.localAbortId.set(this.mAbortId);
+
+                if (LocalSendingMessageProvider.getInstance().updateLocalSendingMessage(
+                        this.mSessionUserId,
+                        localSendingMessageUpdate)) {
+                    final LocalSendingMessage localSendingMessage = LocalSendingMessageProvider.getInstance().getLocalSendingMessage(
+                            this.mSessionUserId,
+                            this.mLocalSendingMessage.localId.get()
+                    );
+                    if (localSendingMessage != null) {
+                        this.mLocalSendingMessage = localSendingMessage;
+                    } else {
+                        setError(ERROR_CODE_TARGET_NOT_FOUND, null);
+                    }
+                } else {
+                    setError(ERROR_CODE_BIND_ABORT_ID_FAIL, null);
+                }
+            } catch (Throwable e) {
+                IMLog.e(e);
+                setError(ERROR_CODE_UNKNOWN, null);
+            }
+        }
+
+        @Nullable
+        private LocalSendingMessage validateAbortIdMatch() {
+            if (this.mAbortIdNotMatch) {
+                return null;
+            }
+            LocalSendingMessage localSendingMessage = LocalSendingMessageProvider.getInstance().getLocalSendingMessage(
+                    this.mSessionUserId,
+                    this.mLocalSendingMessage.localId.get()
+            );
+            if (localSendingMessage == null) {
+                setError(ERROR_CODE_TARGET_NOT_FOUND, null);
+                this.mAbortIdNotMatch = true;
+                return null;
+            }
+            this.mLocalSendingMessage = localSendingMessage;
+
+            if (localSendingMessage.localAbortId.get() != mAbortId) {
+                IMLog.e("abort id changed. %s : %s", localSendingMessage.localAbortId.get(), mAbortId);
+                this.mAbortIdNotMatch = true;
+                return localSendingMessage;
+            }
+            this.mAbortIdNotMatch = false;
+            return localSendingMessage;
+        }
+
+        private void moveSendStatus(int sendStatus) {
+            try {
+                LocalSendingMessage localSendingMessage = validateAbortIdMatch();
+                if (this.mAbortIdNotMatch) {
+                    return;
+                }
+
+                final LocalSendingMessage localSendingMessageUpdate = new LocalSendingMessage();
+                localSendingMessageUpdate.localId.set(this.mLocalSendingMessage.localId.get());
+                localSendingMessageUpdate.localSendStatus.set(sendStatus);
+                if (LocalSendingMessageProvider.getInstance().updateLocalSendingMessage(this.mSessionUserId, localSendingMessageUpdate)) {
+                    localSendingMessage = LocalSendingMessageProvider.getInstance().getLocalSendingMessage(
+                            this.mSessionUserId,
+                            this.mLocalSendingMessage.localId.get()
+                    );
+                    if (localSendingMessage != null) {
+                        this.mLocalSendingMessage = localSendingMessage;
+                    } else {
+                        setError(ERROR_CODE_TARGET_NOT_FOUND, null);
+                    }
+                } else {
+                    setError(ERROR_CODE_UPDATE_SEND_STATUS_FAIL, null);
+                }
+            } catch (Throwable e) {
+                IMLog.e(e);
+                setError(ERROR_CODE_UNKNOWN, null);
+            }
+        }
+    }
+
     private class SessionUploader {
 
         private final long mSessionUserId;
 
         /**
-         * 所有正在执行或等待执行的消息任务
+         * 所有正在执行的消息任务数量上限
          */
-        private static final int MAX_RUNNING_IDLE_SIZE = 10;
+        private static final int MAX_RUNNING_SIZE = 10;
         /**
-         * 以 sign 为 key 的正在执行或等待执行的消息任务
+         * 所有正在执行的消息任务
          */
-        private final LongSparseArray<OneMessageUploadTask> mAllRunningTasks = new LongSparseArray<>();
+        private final List<MessageUploadObjectWrapperTask> mAllRunningTasks = new ArrayList<>();
 
         private final TaskQueue mCheckIdleActionQueue = new TaskQueue(1);
 
@@ -94,56 +275,138 @@ public class IMMessageUploadManager {
             LocalSendingMessageProvider.getInstance().removeAllSuccessMessage(mSessionUserId);
         }
 
+        @Nullable
+        private MessageUploadObjectWrapperTask getTask(@NonNull LocalSendingMessage localSendingMessage) {
+            final long localId = localSendingMessage.localId.get();
+            for (MessageUploadObjectWrapperTask task : mAllRunningTasks) {
+                if (localId == task.mMessageUploadObjectWrapper.mLocalSendingMessage.localId.get()) {
+                    return task;
+                }
+            }
+            return null;
+        }
+
+        @Nullable
+        private MessageUploadObjectWrapperTask removeTask(@NonNull LocalSendingMessage localSendingMessage) {
+            final long localId = localSendingMessage.localId.get();
+            for (int i = 0; i < mAllRunningTasks.size(); i++) {
+                final MessageUploadObjectWrapperTask task = mAllRunningTasks.get(i);
+                if (localId == task.mMessageUploadObjectWrapper.mLocalSendingMessage.localId.get()) {
+                    return mAllRunningTasks.remove(i);
+                }
+            }
+            return null;
+        }
+
         private void dispatchCheckIdleMessage() {
             if (mCheckIdleActionQueue.getWaitCount() > 5) {
                 // 不直接判断 > 0, 做一些冗余，此处不做锁。
                 return;
             }
             mCheckIdleActionQueue.skipQueue();
-            mCheckIdleActionQueue.enqueue(new SafetyRunnable() {
-                @Override
-                protected void onSafetyRun() {
-                    if (mAllRunningTasks.size() >= MAX_RUNNING_IDLE_SIZE) {
-                        IMLog.v("ignore, already has many running task size:%s", mAllRunningTasks.size());
-                        return;
-                    }
+            mCheckIdleActionQueue.enqueue(new SafetyRunnable(() -> {
+                if (mAllRunningTasks.size() >= MAX_RUNNING_SIZE) {
+                    IMLog.v("ignore, already has many running task size:%s", mAllRunningTasks.size());
+                    return;
+                }
 
-                    LocalSendingMessageProvider.getInstance().pageQueryLocalSendingMessage()
+                final List<LocalSendingMessage> localSendingMessageList =
+                        LocalSendingMessageProvider
+                                .getInstance()
+                                .getIdleMessageList(mSessionUserId, MAX_RUNNING_SIZE);
 
-                    List<Message> idleMessageIds = ImMessageDatabaseProvider.getInstance().getIdleMessages(
-                            mSession.sessionUserId,
-                            MAX_RUNNING_IDLE_SIZE);
-                    if (isAbort()) {
-                        return;
-                    }
+                synchronized (mAllRunningTasks) {
+                    for (LocalSendingMessage localSendingMessage : localSendingMessageList) {
+                        final MessageUploadObjectWrapperTask oldTask = getTask(localSendingMessage);
+                        if (oldTask != null) {
+                            final Throwable e = new IllegalAccessError("unexpected localSendingMessage already exists in task. "
+                                    + localSendingMessage + ", oldTask:" + Objects.defaultObjectTag(oldTask));
+                            IMLog.e(e);
+                            continue;
+                        }
 
-                    int foundNewIdleMessageSize = 0;
-                    for (Message item : idleMessageIds) {
-                        if (!mAllRunningTasks.containsKey(item.id)) {
-                            foundNewIdleMessageSize++;
+                        IMLog.v("found new idle localSendingMessage %s", localSendingMessage);
+                        final long sign = SignGenerator.next();
+                        final MessageUploadObjectWrapper wrapper = new MessageUploadObjectWrapper(
+                                mSessionUserId,
+                                sign,
+                                sign,
+                                localSendingMessage
+                        );
+                        wrapper.mMessage = MessageDatabaseProvider.getInstance().getMessage(
+                                mSessionUserId,
+                                localSendingMessage.conversationType.get(),
+                                localSendingMessage.targetUserId.get(),
+                                localSendingMessage.messageLocalId.get()
+                        );
+                        wrapper.bindAbortId();
 
-                            final long localMessageId = item.id;
-                            OneMessageUploadTask task = new OneMessageUploadTask(mSession, item.id) {
-                                @Override
-                                protected void onRunEnd() {
-                                    super.onRunEnd();
-                                    mAllRunningTasks.remove(localMessageId);
-                                    dispatchCheckIdleMessage();
+                        final MessageUploadObjectWrapperTask task = new MessageUploadObjectWrapperTask(wrapper) {
+                            @Override
+                            public void run() {
+                                try {
+                                    super.run();
+                                } catch (Throwable e) {
+                                    IMLog.e(e, "unexpected");
                                 }
-                            };
-                            mAllRunningTasks.put(item.id, task);
-
-                            if (isLongTimeMessageType(item.msgType)) {
-                                mLongTimeTaskQueue.enqueue(task);
-                            } else {
-                                mShortTimeTaskQueue.enqueue(task);
+                                wrapper.onTaskEnd();
+                                synchronized (mAllRunningTasks) {
+                                    final MessageUploadObjectWrapperTask existsTask = removeTask(localSendingMessage);
+                                    if (existsTask == null) {
+                                        IMLog.e("unexpected removeTask return null %s", localSendingMessage);
+                                    } else if (existsTask != this) {
+                                        IMLog.e("unexpected removeTask return another value %s", localSendingMessage);
+                                    } else {
+                                        IMLog.v("success remove task %s", localSendingMessage);
+                                    }
+                                }
+                                dispatchCheckIdleMessage();
                             }
+                        };
+                        mAllRunningTasks.add(task);
+                        if (wrapper.isFastMessage()) {
+                            mShortTimeTaskQueue.enqueue(task);
+                        } else {
+                            mLongTimeTaskQueue.enqueue(task);
                         }
                     }
-
-                    Timber.v("found %s idle messages add to upload task, all running task size:%s", foundNewIdleMessageSize, mAllRunningTasks.size());
                 }
-            });
+
+                IMLog.v("checkIdleActionQueue run localSendingMessageList size:%s, allRunningTasks size:%s", localSendingMessageList.size(), mAllRunningTasks.size());
+            }));
+        }
+
+        private class MessageUploadObjectWrapperTask implements Runnable {
+
+            @NonNull
+            private final MessageUploadObjectWrapper mMessageUploadObjectWrapper;
+
+            private MessageUploadObjectWrapperTask(MessageUploadObjectWrapper messageUploadObjectWrapper) {
+                mMessageUploadObjectWrapper = messageUploadObjectWrapper;
+            }
+
+            @Override
+            public void run() {
+                try {
+                    LocalSendingMessage localSendingMessage = mMessageUploadObjectWrapper.validateAbortIdMatch();
+                    if (mMessageUploadObjectWrapper.mAbortIdNotMatch) {
+                        return;
+                    }
+                    mMessageUploadObjectWrapper.moveSendStatus(IMConstants.SendStatus.SENDING);
+
+                    final Message message = mMessageUploadObjectWrapper.mMessage;
+                    if (message == null) {
+                        mMessageUploadObjectWrapper.setError(MessageUploadObjectWrapper.ERROR_CODE_TARGET_NOT_FOUND, null);
+                        return;
+                    }
+
+                    // TODO message to proto buf ?
+                } catch (Throwable e) {
+                    IMLog.e(e);
+                    mMessageUploadObjectWrapper.setError(MessageUploadObjectWrapper.ERROR_CODE_UNKNOWN, null);
+                }
+            }
+
         }
 
         private class OneMessageUploadTask extends AbortRunnable {
