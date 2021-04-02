c7727aa66d16facc5afa80546175aba082ae8bd7
idonans <idonans@126.com>
2021-04-02 08:42:24 +0800

    fix IMMessageTextSendViewHolder avatar style

--- app/src/main/java/com/masonsoft/imsdk/sample/uniontype/viewholder/IMMessageTextSendViewHolder.java
+++ app/src/main/java/com/masonsoft/imsdk/sample/uniontype/viewholder/IMMessageTextSendViewHolder.java
@@ -1,11 +1,17 @@
 package com.masonsoft.imsdk.sample.uniontype.viewholder;
 
+import android.app.Activity;
+
 import androidx.annotation.CallSuper;
 import androidx.annotation.NonNull;
 
+import com.idonans.lang.util.ViewUtil;
 import com.idonans.uniontype.Host;
 import com.masonsoft.imsdk.IMMessage;
+import com.masonsoft.imsdk.core.IMLog;
+import com.masonsoft.imsdk.sample.Constants;
 import com.masonsoft.imsdk.sample.R;
+import com.masonsoft.imsdk.sample.SampleLog;
 import com.masonsoft.imsdk.sample.databinding.ImsdkSampleUnionTypeImMessageTextSendBinding;
 import com.masonsoft.imsdk.sample.uniontype.DataObject;
 
@@ -26,6 +32,20 @@ public class IMMessageTextSendViewHolder extends IMMessageTextViewHolder {
         final DataObject<IMMessage> itemObject = (DataObject<IMMessage>) originObject;
         final IMMessage imMessage = itemObject.object;
         mBinding.sendStatusView.setMessage(imMessage);
+
+        mBinding.avatar.setTargetUserId(imMessage.fromUserId.getOrDefault(0L));
+        mBinding.avatar.setShowBorder(false);
+
+        ViewUtil.onClick(mBinding.avatar, v -> {
+            Activity innerActivity = host.getActivity();
+            if (innerActivity == null) {
+                SampleLog.e(Constants.ErrorLog.ACTIVITY_IS_NULL);
+                return;
+            }
+
+            // TODO open profile ?
+            IMLog.w("require open profile");
+        });
     }
 
 }
