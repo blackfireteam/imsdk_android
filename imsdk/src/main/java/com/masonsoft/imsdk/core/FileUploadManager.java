6df04f89d3afae4f19d9202ca407f5b48637aaa3
idonans <idonans@126.com>
2021-04-02 11:46:55 +0800

    add FileUploadManager.MemoryFullCache

--- imsdk/src/main/java/com/masonsoft/imsdk/core/FileUploadManager.java
+++ imsdk/src/main/java/com/masonsoft/imsdk/core/FileUploadManager.java
@@ -2,9 +2,11 @@ package com.masonsoft.imsdk.core;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.collection.LruCache;
 
 import com.idonans.core.Progress;
 import com.idonans.core.Singleton;
+import com.masonsoft.imsdk.util.Objects;
 
 /**
  * 文件上传管理
@@ -22,6 +24,28 @@ public class FileUploadManager {
 
     public static FileUploadManager getInstance() {
         return INSTANCE.get();
+    }
+
+    private static class MemoryFullCache {
+
+        private static final MemoryFullCache DEFAULT = new MemoryFullCache();
+
+        private static final int MEMORY_CACHE_SIZE = 100;
+        @NonNull
+        private final LruCache<String, String> mFullCaches = new LruCache<>(MEMORY_CACHE_SIZE);
+
+        private void addFullCache(@NonNull String filePath, @NonNull String accessUrl) {
+            mFullCaches.put(filePath, accessUrl);
+        }
+
+        private void removeFullCache(@NonNull String filePath) {
+            mFullCaches.remove(filePath);
+        }
+
+        @Nullable
+        private String getFullCache(@NonNull String filePath) {
+            return mFullCaches.get(filePath);
+        }
     }
 
     @NonNull
@@ -51,9 +75,18 @@ public class FileUploadManager {
         @NonNull
         @Override
         public String uploadFile(@NonNull String filePath, @NonNull Progress progress) throws Throwable {
+            final String cache = MemoryFullCache.DEFAULT.getFullCache(filePath);
+            if (cache != null) {
+                IMLog.v(Objects.defaultObjectTag(this) + " uploadFile cache hit. %s -> %s",
+                        filePath, cache);
+                return cache;
+            }
+
             final FileUploadProvider provider = mProvider;
             if (provider != null) {
-                return provider.uploadFile(filePath, progress);
+                final String accessUrl = provider.uploadFile(filePath, progress);
+                MemoryFullCache.DEFAULT.addFullCache(filePath, accessUrl);
+                return accessUrl;
             }
             throw new IllegalAccessError("provider not found");
         }
