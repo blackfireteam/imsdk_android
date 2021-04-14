8b81441f901942fd94d0e42d78d299e1c0720cff
idonans <idonans@126.com>
2021-04-14 14:28:26 +0800

    add ActionMessageObservable

---
+++ imsdk/src/main/java/com/masonsoft/imsdk/core/observable/ActionMessageObservable.java
@@ -0,0 +1,32 @@
+package com.masonsoft.imsdk.core.observable;
+
+import com.masonsoft.imsdk.util.WeakObservable;
+
+/**
+ * 指令消息
+ */
+public class ActionMessageObservable extends WeakObservable<ActionMessageObservable.ActionMessageObserver> {
+
+    public static final ActionMessageObservable DEFAULT = new ActionMessageObservable();
+
+    public interface ActionMessageObserver {
+        void onActionMessageLoading(long sign);
+
+        void onActionSuccess(long sign);
+
+        void onActionMessageError(long sign, long errorCode, String errorMessage);
+    }
+
+    public void notifyActionMessageLoading(long sign) {
+        forEach(actionMessageObserver -> actionMessageObserver.onActionMessageLoading(sign));
+    }
+
+    public void notifyActionMessageSuccess(long sign) {
+        forEach(actionMessageObserver -> actionMessageObserver.onActionSuccess(sign));
+    }
+
+    public void notifyActionMessageError(long sign, long errorCode, String errorMessage) {
+        forEach(actionMessageObserver -> actionMessageObserver.onActionMessageError(sign, errorCode, errorMessage));
+    }
+
+}
