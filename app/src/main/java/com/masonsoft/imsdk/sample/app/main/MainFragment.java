cb326332405aa4e08138919a90a455a13ff02b9e
idonans <idonans@126.com>
2021-04-16 14:38:17 +0800

    improve MainFragment layout

--- app/src/main/java/com/masonsoft/imsdk/sample/app/main/MainFragment.java
+++ app/src/main/java/com/masonsoft/imsdk/sample/app/main/MainFragment.java
@@ -5,12 +5,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.IntDef;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.viewpager2.adapter.FragmentStateAdapter;
-import androidx.viewpager2.widget.ViewPager2;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.Lifecycle;
 
+import com.masonsoft.imsdk.sample.Constants;
 import com.masonsoft.imsdk.sample.R;
 import com.masonsoft.imsdk.sample.SampleLog;
 import com.masonsoft.imsdk.sample.app.SystemInsetsFragment;
@@ -19,8 +22,6 @@ import com.masonsoft.imsdk.sample.app.discover.DiscoverFragment;
 import com.masonsoft.imsdk.sample.app.home.HomeFragment;
 import com.masonsoft.imsdk.sample.app.mine.MineFragment;
 import com.masonsoft.imsdk.sample.databinding.ImsdkSampleMainFragmentBinding;
-
-import io.github.idonans.systeminsets.SystemInsetsLayout;
 
 public class MainFragment extends SystemInsetsFragment {
 
@@ -33,39 +34,39 @@ public class MainFragment extends SystemInsetsFragment {
 
     @Nullable
     private ImsdkSampleMainFragmentBinding mBinding;
-    private final int[] mTitleResIds = {
-            R.string.imsdk_sample_tab_home,
-            R.string.imsdk_sample_tab_discover,
-            R.string.imsdk_sample_tab_conversation,
-            R.string.imsdk_sample_tab_mine
-    };
+
+    private static final int FRAGMENT_ID_HOME = 0;
+    private static final int FRAGMENT_ID_DISCOVER = 1;
+    private static final int FRAGMENT_ID_CONVERSATION = 2;
+    private static final int FRAGMENT_ID_MINE = 3;
+
+    @IntDef({FRAGMENT_ID_HOME, FRAGMENT_ID_DISCOVER, FRAGMENT_ID_CONVERSATION, FRAGMENT_ID_MINE})
+    private @interface FragmentId {
+    }
+
+    private int mCurrentFragmentId = -1;
+
+    private static final String FRAGMENT_TAG_HOME = "fragment_home_20210416";
+    private static final String FRAGMENT_TAG_DISCOVER = "fragment_discover_20210416";
+    private static final String FRAGMENT_TAG_CONVERSATION = "fragment_conversation_20210416";
+    private static final String FRAGMENT_TAG_MINE = "fragment_mine_20210416";
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         mBinding = ImsdkSampleMainFragmentBinding.inflate(inflater, container, false);
-        mBinding.pager.setOffscreenPageLimit(3);
-        mBinding.pager.setAdapter(new DataAdapter());
-        mBinding.pager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
-            @Override
-            public void onPageSelected(int position) {
-                syncTabSelected(position);
-            }
-        });
-        mBinding.mainBottomBar.setOnTabClickListener(this::syncTabSelected);
-        syncTabSelected(0);
-
-        mBinding.mainTopBarContainer.setOnSystemInsetsListener(new SystemInsetsLayout.OnSystemInsetsListener() {
-            @Override
-            public void onSystemInsets(int left, int top, int right, int bottom) {
-                SampleLog.v("mainTopBarContainer onSystemInsets: left:%s, top:%s, right:%s, bottom:%s", left, top, right, bottom);
-            }
-        });
         mBinding.mainBottomBarContainer.setOnSystemInsetsListener((left, top, right, bottom) ->
                 SampleLog.v("mainBottomBarContainer onSystemInsets: left:%s, top:%s, right:%s, bottom:%s", left, top, right, bottom)
         );
-
         return mBinding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        mBinding.mainBottomBar.setOnTabClickListener(this::syncTabSelected);
+        syncTabSelected(0);
+
+        super.onViewCreated(view, savedInstanceState);
     }
 
     @Override
@@ -78,41 +79,133 @@ public class MainFragment extends SystemInsetsFragment {
     private void syncTabSelected(int index) {
         SampleLog.v("syncTabSelected index:%s", index);
         if (mBinding != null) {
-            mBinding.mainTopBar.setTitle(getString(mTitleResIds[index]));
-            if (mBinding.pager.getCurrentItem() != index) {
-                mBinding.pager.setCurrentItem(index, false);
-            }
             if (mBinding.mainBottomBar.getCurrentItem() != index) {
                 mBinding.mainBottomBar.setCurrentItem(index);
             }
+            showFragment(index);
         }
     }
 
-    private class DataAdapter extends FragmentStateAdapter {
-        public DataAdapter() {
-            super(MainFragment.this);
+    private void showFragment(@FragmentId int fragmentId) {
+        if (mCurrentFragmentId == fragmentId) {
+            SampleLog.v("ignore. showFragment mCurrentFragmentId is already %s", fragmentId);
+            return;
         }
 
-        @NonNull
-        @Override
-        public Fragment createFragment(int position) {
-            if (position == 0) {
-                return HomeFragment.newInstance();
-            } else if (position == 1) {
-                return DiscoverFragment.newInstance();
-            } else if (position == 2) {
-                return ConversationFragment.newInstance();
-            } else if (position == 3) {
-                return MineFragment.newInstance();
+
+        FragmentManager fm = getChildFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        boolean needCommit = false;
+
+        if (fm.isStateSaved()) {
+            SampleLog.e(Constants.ErrorLog.FRAGMENT_MANAGER_STATE_SAVED);
+            return;
+        }
+
+        final Fragment homeFragment = getFragment(FRAGMENT_ID_HOME, fragmentId == FRAGMENT_ID_HOME);
+        final Fragment discoverFragment = getFragment(FRAGMENT_ID_DISCOVER, fragmentId == FRAGMENT_ID_DISCOVER);
+        final Fragment conversationFragment = getFragment(FRAGMENT_ID_CONVERSATION, fragmentId == FRAGMENT_ID_CONVERSATION);
+        final Fragment mineFragment = getFragment(FRAGMENT_ID_MINE, fragmentId == FRAGMENT_ID_MINE);
+
+        if (homeFragment != null) {
+            if (fragmentId == FRAGMENT_ID_HOME) {
+                ft.show(homeFragment).setMaxLifecycle(homeFragment, Lifecycle.State.RESUMED);
             } else {
-                throw new IllegalArgumentException("unexpected position:" + position);
+                ft.hide(homeFragment).setMaxLifecycle(homeFragment, Lifecycle.State.STARTED);
             }
+            needCommit = true;
         }
 
-        @Override
-        public int getItemCount() {
-            return mTitleResIds.length;
+        if (discoverFragment != null) {
+            if (fragmentId == FRAGMENT_ID_DISCOVER) {
+                ft.show(discoverFragment).setMaxLifecycle(discoverFragment, Lifecycle.State.RESUMED);
+            } else {
+                ft.hide(discoverFragment).setMaxLifecycle(discoverFragment, Lifecycle.State.STARTED);
+            }
+            needCommit = true;
         }
+
+        if (conversationFragment != null) {
+            if (fragmentId == FRAGMENT_ID_CONVERSATION) {
+                ft.show(conversationFragment).setMaxLifecycle(conversationFragment, Lifecycle.State.RESUMED);
+            } else {
+                ft.hide(conversationFragment).setMaxLifecycle(conversationFragment, Lifecycle.State.STARTED);
+            }
+            needCommit = true;
+        }
+
+        if (mineFragment != null) {
+            if (fragmentId == FRAGMENT_ID_MINE) {
+                ft.show(mineFragment).setMaxLifecycle(mineFragment, Lifecycle.State.RESUMED);
+            } else {
+                ft.hide(mineFragment).setMaxLifecycle(mineFragment, Lifecycle.State.STARTED);
+            }
+            needCommit = true;
+        }
+
+        if (needCommit) {
+            mCurrentFragmentId = fragmentId;
+            ft.commitNow();
+        }
+    }
+
+    private static String getFragmentTag(@FragmentId int fragmentId) {
+        if (fragmentId == FRAGMENT_ID_HOME) {
+            return FRAGMENT_TAG_HOME;
+        }
+        if (fragmentId == FRAGMENT_ID_DISCOVER) {
+            return FRAGMENT_TAG_DISCOVER;
+        }
+        if (fragmentId == FRAGMENT_ID_CONVERSATION) {
+            return FRAGMENT_TAG_CONVERSATION;
+        }
+        if (fragmentId == FRAGMENT_ID_MINE) {
+            return FRAGMENT_TAG_MINE;
+        }
+        final Throwable e = new IllegalArgumentException("unexpected fragmentId:" + fragmentId);
+        throw new RuntimeException(e);
+    }
+
+    private static Fragment newFragment(@FragmentId int fragmentId) {
+        if (fragmentId == FRAGMENT_ID_HOME) {
+            return HomeFragment.newInstance();
+        }
+        if (fragmentId == FRAGMENT_ID_DISCOVER) {
+            return DiscoverFragment.newInstance();
+        }
+        if (fragmentId == FRAGMENT_ID_CONVERSATION) {
+            return ConversationFragment.newInstance();
+        }
+        if (fragmentId == FRAGMENT_ID_MINE) {
+            return MineFragment.newInstance();
+        }
+        final Throwable e = new IllegalArgumentException("unexpected fragmentId:" + fragmentId);
+        throw new RuntimeException(e);
+    }
+
+    @Nullable
+    private Fragment getFragment(@FragmentId int fragmentId, boolean create) {
+        FragmentManager fm = getChildFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        boolean needCommit = false;
+
+        if (fm.isStateSaved()) {
+            SampleLog.e(Constants.ErrorLog.FRAGMENT_MANAGER_STATE_SAVED);
+            return null;
+        }
+
+        final String fragmentTag = getFragmentTag(fragmentId);
+        Fragment fragment = fm.findFragmentByTag(fragmentTag);
+        if (fragment == null && create) {
+            fragment = newFragment(fragmentId);
+            ft.add(R.id.system_insets_layer_layout, fragment, fragmentTag);
+            needCommit = true;
+        }
+
+        if (needCommit) {
+            ft.commitNow();
+        }
+        return fragment;
     }
 
 }
