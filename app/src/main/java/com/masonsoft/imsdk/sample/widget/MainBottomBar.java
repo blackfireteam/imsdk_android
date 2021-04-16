6467fcfac95a337b8ed33a4a4ef33694af4aac99
idonans <idonans@126.com>
2021-04-16 10:01:29 +0800

    fix MainBottomBar

--- app/src/main/java/com/masonsoft/imsdk/sample/widget/MainBottomBar.java
+++ app/src/main/java/com/masonsoft/imsdk/sample/widget/MainBottomBar.java
@@ -34,9 +34,10 @@ public class MainBottomBar extends FrameLayout {
         initFromAttributes(context, attrs, defStyleAttr, defStyleRes);
     }
 
-    private static final int TAB_DISCOVER = 0;
-    private static final int TAB_CONVERSATION = 1;
-    private static final int TAB_MINE = 2;
+    private static final int TAB_HOME = 0;
+    private static final int TAB_DISCOVER = 1;
+    private static final int TAB_CONVERSATION = 2;
+    private static final int TAB_MINE = 3;
 
     private int mCurrentItem = 0;
     private ImsdkSampleWidgetMainBottomBarBinding mBinding;
@@ -54,6 +55,11 @@ public class MainBottomBar extends FrameLayout {
                 true);
         syncCurrentItem();
 
+        ViewUtil.onClick(mBinding.tabHomeText, v -> {
+            if (mOnTabClickListener != null) {
+                mOnTabClickListener.onTabClick(TAB_HOME);
+            }
+        });
         ViewUtil.onClick(mBinding.tabDiscoverText, v -> {
             if (mOnTabClickListener != null) {
                 mOnTabClickListener.onTabClick(TAB_DISCOVER);
@@ -84,9 +90,10 @@ public class MainBottomBar extends FrameLayout {
     }
 
     private void syncCurrentItem() {
-        mBinding.tabDiscoverText.setSelected(mCurrentItem == 0);
-        mBinding.tabConversationText.setSelected(mCurrentItem == 1);
-        mBinding.tabMineText.setSelected(mCurrentItem == 2);
+        mBinding.tabHomeText.setSelected(mCurrentItem == TAB_HOME);
+        mBinding.tabDiscoverText.setSelected(mCurrentItem == TAB_DISCOVER);
+        mBinding.tabConversationText.setSelected(mCurrentItem == TAB_CONVERSATION);
+        mBinding.tabMineText.setSelected(mCurrentItem == TAB_MINE);
     }
 
     public void setOnTabClickListener(OnTabClickListener onTabClickListener) {
