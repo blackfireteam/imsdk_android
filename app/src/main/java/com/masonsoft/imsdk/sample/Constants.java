e2ae921ae690d9052b6cc8aca80aed03f72afcd5
idonans <idonans@126.com>
2021-04-25 12:13:44 +0800

    set default DEBUG_WIDGET as false

--- app/src/main/java/com/masonsoft/imsdk/sample/Constants.java
+++ app/src/main/java/com/masonsoft/imsdk/sample/Constants.java
@@ -6,7 +6,7 @@ import io.github.idonans.core.util.HumanUtil;
 
 public class Constants {
 
-    public static final boolean DEBUG_WIDGET = true;
+    public static final boolean DEBUG_WIDGET = false;
 
     public static final String APP_SECRET = "asfasdasd123";
     public static final String SAMPLE_STORAGE_NAMESPACE = "imsdk_sample_storage_20210420";
