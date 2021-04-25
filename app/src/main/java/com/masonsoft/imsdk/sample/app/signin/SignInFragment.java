d8320322405b67896c7baaf2ebc5a4b391c700cc
idonans <idonans@126.com>
2021-04-25 14:36:35 +0800

    adjust sign in fragment default server local value

--- app/src/main/java/com/masonsoft/imsdk/sample/app/signin/SignInFragment.java
+++ app/src/main/java/com/masonsoft/imsdk/sample/app/signin/SignInFragment.java
@@ -39,8 +39,8 @@ public class SignInFragment extends SystemInsetsFragment {
 
     private static final String DEFAULT_API_SERVER_INTERNET = "https://im.ekfree.com:18788";
     private static final String DEFAULT_IM_SERVER_INTERNET = "im.ekfree.com:18888";
-    private static final String DEFAULT_API_SERVER_LOCAL = "https://192.168.50.254:18788";
-    private static final String DEFAULT_IM_SERVER_LOCAL = "192.168.50.254:18888";
+    private static final String DEFAULT_API_SERVER_LOCAL = "https://192.168.50.253:18788";
+    private static final String DEFAULT_IM_SERVER_LOCAL = "192.168.50.253:18888";
     private boolean mCurrentApiServerInternet = true;
 
     @Nullable
