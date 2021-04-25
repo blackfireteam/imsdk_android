01238fed605ae6f65c0e75366e5343c19d68eb4d
idonans <idonans@126.com>
2021-04-25 12:10:36 +0800

    add DebugTextView gone if not allow debug widget

--- app/src/main/java/com/masonsoft/imsdk/sample/widget/debug/DebugTextView.java
+++ app/src/main/java/com/masonsoft/imsdk/sample/widget/debug/DebugTextView.java
@@ -2,10 +2,15 @@ package com.masonsoft.imsdk.sample.widget.debug;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.AppCompatTextView;
+
+import com.masonsoft.imsdk.sample.Constants;
+
+import io.github.idonans.lang.util.ViewUtil;
 
 class DebugTextView extends AppCompatTextView {
 
@@ -29,6 +34,10 @@ class DebugTextView extends AppCompatTextView {
         this.setIncludeFontPadding(false);
         this.setTextColor(0xffff0000);
         this.setBackgroundColor(0x90999999);
+
+        if (!Constants.DEBUG_WIDGET) {
+            ViewUtil.setVisibilityIfChanged(this, View.GONE);
+        }
     }
 
 }
