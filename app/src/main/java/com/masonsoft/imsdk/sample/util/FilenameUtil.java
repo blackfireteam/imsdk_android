ed58c1aed01bbc5c9ddce409476be60d3f730b85
idonans <idonans@126.com>
2021-05-11 10:52:50 +0800

    update filename util null extension

--- app/src/main/java/com/masonsoft/imsdk/sample/util/FilenameUtil.java
+++ app/src/main/java/com/masonsoft/imsdk/sample/util/FilenameUtil.java
@@ -1,11 +1,17 @@
 package com.masonsoft.imsdk.sample.util;
 
+import androidx.annotation.Nullable;
+
 import java.util.UUID;
 
 public class FilenameUtil {
 
-    public static String createUnionFilename(String fileExtension) {
-        return UUID.randomUUID().toString().replace("-", "") + "." + fileExtension.toLowerCase();
+    public static String createUnionFilename(@Nullable String fileExtension) {
+        String filename = UUID.randomUUID().toString().replace("-", "");
+        if (fileExtension != null) {
+            filename += "." + fileExtension.toLowerCase();
+        }
+        return filename;
     }
 
 }
