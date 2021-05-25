cfe73b6be8526106ea172c68ec630f349fd51ccc
idonans <idonans@126.com>
2021-05-25 10:39:25 +0800

    define MSIMMessage

--- README.md
+++ README.md
@@ -1,9 +1,6 @@
-## MSIMSDK for Android
+# MSIMSDK for Android
 
-maven 集成
-在 gradle 里添加如下依赖
-```gradle
-dependencies {
-    implementation 'io.github.blackfireteam.msimsdk:msimsdk:1.0-SNAPSHOT'
-}
+## 引入依赖
+```
+    implementation 'io.github.blackfireteam.msimsdk:msimsdk:$latestVersion'
 ```
---
+++ imsdk/src/main/java/com/masonsoft/imsdk/MSIMMessage.java
@@ -0,0 +1,20 @@
+package com.masonsoft.imsdk;
+
+import androidx.annotation.NonNull;
+
+import com.masonsoft.imsdk.core.IMMessage;
+
+/**
+ * @since 1.0
+ */
+public class MSIMMessage {
+
+    @NonNull
+    private IMMessage mMessage;
+
+    public long getMessageId() {
+        // TODO
+        return 0L;
+    }
+
+}
