e6af109103c6940c7d349141642b918719a138d6
idonans <idonans@126.com>
2021-05-28 17:29:00 +0800

    add always clear local unread count first

--- imsdk/src/main/java/com/masonsoft/imsdk/core/processor/SendActionTypeMarkAsReadValidateProcessor.java
+++ imsdk/src/main/java/com/masonsoft/imsdk/core/processor/SendActionTypeMarkAsReadValidateProcessor.java
@@ -4,6 +4,9 @@ import androidx.annotation.NonNull;
 
 import com.masonsoft.imsdk.core.IMActionMessage;
 import com.masonsoft.imsdk.core.IMActionMessageManager;
+import com.masonsoft.imsdk.core.IMConstants;
+import com.masonsoft.imsdk.core.db.Conversation;
+import com.masonsoft.imsdk.core.db.ConversationDatabaseProvider;
 import com.masonsoft.imsdk.lang.GeneralResult;
 
 /**
@@ -31,6 +34,17 @@ public class SendActionTypeMarkAsReadValidateProcessor extends SendActionTypeVal
                             .withPayload(target)
             );
             return true;
+        }
+
+        {
+            // 手动清空本地未读消息数
+            final Conversation conversation = ConversationDatabaseProvider.getInstance().getConversationByTargetUserId(target.getSessionUserId(), IMConstants.ConversationType.C2C, targetUserId);
+            if (conversation != null) {
+                final Conversation conversationUpdate = new Conversation();
+                conversationUpdate.localId.set(conversation.localId.get());
+                conversationUpdate.localUnreadCount.set(0L);
+                ConversationDatabaseProvider.getInstance().updateConversation(target.getSessionUserId(), conversationUpdate);
+            }
         }
 
         // 提示成功入队
