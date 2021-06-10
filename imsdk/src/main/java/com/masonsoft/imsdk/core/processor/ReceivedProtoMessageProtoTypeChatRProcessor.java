b286ed243aac5ba583a2a473a57a72de371a2885
idonans <idonans@126.com>
2021-06-10 14:09:03 +0800

    replace old ReceivedProtoMessageProtoTypeChatRProcessor

--- imsdk/src/main/java/com/masonsoft/imsdk/core/processor/ReceivedProtoMessageProtoTypeChatRProcessor.java
+++ imsdk/src/main/java/com/masonsoft/imsdk/core/processor/ReceivedProtoMessageProtoTypeChatRProcessor.java
@@ -13,7 +13,8 @@ import com.masonsoft.imsdk.util.TimeDiffDebugHelper;
 import java.util.ArrayList;
 import java.util.List;
 
-import io.github.idonans.core.thread.TaskQueue;
+import io.github.idonans.core.thread.BatchQueue;
+import io.github.idonans.core.util.Preconditions;
 
 /**
  * 收到一条新的聊天消息
@@ -23,129 +24,61 @@ import io.github.idonans.core.thread.TaskQueue;
 public class ReceivedProtoMessageProtoTypeChatRProcessor extends ReceivedProtoMessageProtoTypeProcessor<ProtoMessage.ChatR> {
 
     private final TimeDiffDebugHelper mTimeDiffDebugHelper = new TimeDiffDebugHelper(Objects.defaultObjectTag(this));
-    private final Object mBatchLock = new Object();
-    private Batch mCurrentBatch;
+    private final BatchQueue<SessionProtoByteMessageWrapper> mBatchQueue = new BatchQueue<>();
 
     public ReceivedProtoMessageProtoTypeChatRProcessor() {
         super(ProtoMessage.ChatR.class);
+
+        mBatchQueue.setConsumer(payloadList -> {
+            if (payloadList == null || payloadList.isEmpty()) {
+                return;
+            }
+
+            // 如果连续的 SessionTcpClient 与 session user id 相同，则合并处理
+            SessionTcpClient currentSessionTcpClient = null;
+            long currentSessionUserId = -1;
+            List<SessionProtoByteMessageWrapper> currentPayloadList = null;
+            for (SessionProtoByteMessageWrapper payload : payloadList) {
+                if (currentSessionTcpClient == payload.getSessionTcpClient()
+                        && currentSessionUserId == payload.getSessionUserId()) {
+                    Preconditions.checkNotNull(currentPayloadList);
+                } else {
+                    if (currentPayloadList != null) {
+                        processBatch(currentPayloadList);
+                    }
+
+                    currentSessionTcpClient = payload.getSessionTcpClient();
+                    currentSessionUserId = payload.getSessionUserId();
+                    currentPayloadList = new ArrayList<>();
+                }
+                currentPayloadList.add(payload);
+            }
+
+            Preconditions.checkNotNull(currentPayloadList);
+            processBatch(currentPayloadList);
+        });
+    }
+
+    private void processBatch(@NonNull List<SessionProtoByteMessageWrapper> targetList) {
+        mTimeDiffDebugHelper.mark();
+        // 批量处理
+        try {
+            final TinyChatRNewMessageListProcessor processor = new TinyChatRNewMessageListProcessor();
+            processor.doProcess(targetList);
+        } catch (Throwable e) {
+            IMLog.e(e);
+            RuntimeMode.fixme(e);
+        }
+        mTimeDiffDebugHelper.mark();
+        mTimeDiffDebugHelper.print("batch size:" + targetList.size());
     }
 
     @Override
     protected boolean doNotNullProtoMessageObjectProcess(
             @NonNull SessionProtoByteMessageWrapper target,
             @NonNull ProtoMessage.ChatR protoMessageObject) {
-        mTimeDiffDebugHelper.mark();
-        synchronized (mBatchLock) {
-            if (mCurrentBatch != null) {
-                if (mCurrentBatch.merge(target)) {
-                    mCurrentBatch.doNextBatch();
-                    return true;
-                } else {
-                    mCurrentBatch.join();
-                    mCurrentBatch = null;
-                }
-            }
-
-            mCurrentBatch = new Batch(target);
-            mCurrentBatch.doNextBatch();
-        }
-        mTimeDiffDebugHelper.mark();
-        mTimeDiffDebugHelper.print();
+        mBatchQueue.add(target);
         return true;
-    }
-
-    private static final class Batch {
-
-        private final TimeDiffDebugHelper mTimeDiffDebugHelper = new TimeDiffDebugHelper(Objects.defaultObjectTag(this));
-
-        @NonNull
-        private final SessionTcpClient mSessionTcpClient;
-        private final long mSessionUserId;
-
-        private final Object mJoinLock = new Object();
-        private boolean mJoined;
-        private final Object mEndLock = new Object();
-        private boolean mEnd;
-        private final Object mTargetListLock = new Object();
-        private List<SessionProtoByteMessageWrapper> mTargetList = new ArrayList<>();
-        private final TaskQueue mBatchQueue = new TaskQueue(1);
-
-        private Batch(@NonNull SessionProtoByteMessageWrapper target) {
-            mSessionTcpClient = target.getSessionTcpClient();
-            mSessionUserId = target.getSessionUserId();
-            mTargetList.add(target);
-        }
-
-        private boolean merge(@NonNull SessionProtoByteMessageWrapper target) {
-            synchronized (mJoinLock) {
-                if (!mJoined) {
-                    if (mSessionTcpClient == target.getSessionTcpClient()
-                            && mSessionUserId == target.getSessionUserId()) {
-                        synchronized (mTargetListLock) {
-                            mTargetList.add(target);
-                        }
-                        return true;
-                    }
-                    return false;
-                }
-            }
-            return false;
-        }
-
-        private void doNextBatch() {
-            mBatchQueue.skipQueue();
-            mBatchQueue.enqueue(() -> {
-                final List<SessionProtoByteMessageWrapper> targetList;
-                synchronized (mTargetListLock) {
-                    targetList = mTargetList;
-                    mTargetList = new ArrayList<>();
-                }
-                final boolean noMoreTarget = targetList == null || targetList.isEmpty();
-                synchronized (mJoinLock) {
-                    if (mJoined && noMoreTarget) {
-                        synchronized (mEndLock) {
-                            mEnd = true;
-                            mEndLock.notify();
-                        }
-                    }
-                }
-                if (noMoreTarget) {
-                    return;
-                }
-
-                mTimeDiffDebugHelper.mark();
-                // 批量处理
-                try {
-                    final TinyChatRNewMessageListProcessor processor = new TinyChatRNewMessageListProcessor();
-                    processor.doProcess(targetList);
-                } catch (Throwable e) {
-                    IMLog.e(e);
-                    RuntimeMode.fixme(e);
-                }
-                mTimeDiffDebugHelper.mark();
-                mTimeDiffDebugHelper.print("batch size:" + targetList.size());
-                doNextBatch();
-            });
-        }
-
-        private void join() {
-            synchronized (mJoinLock) {
-                mJoined = true;
-            }
-            while (true) {
-                synchronized (mEndLock) {
-                    if (mEnd) {
-                        return;
-                    }
-                    try {
-                        mEndLock.wait(1000L);
-                    } catch (Throwable e) {
-                        // ignore
-                    }
-                }
-            }
-        }
-
     }
 
 }
