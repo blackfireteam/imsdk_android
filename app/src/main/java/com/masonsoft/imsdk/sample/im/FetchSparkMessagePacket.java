13941df9ca8a9ae4c6d2972459f7305e11f4facb
idonans <idonans@126.com>
2021-06-23 11:49:45 +0800

    adjust FetchSparkMessagePacket use SampleLog

--- app/src/main/java/com/masonsoft/imsdk/sample/im/FetchSparkMessagePacket.java
+++ app/src/main/java/com/masonsoft/imsdk/sample/im/FetchSparkMessagePacket.java
@@ -2,11 +2,11 @@ package com.masonsoft.imsdk.sample.im;
 
 import androidx.annotation.NonNull;
 
-import com.masonsoft.imsdk.core.IMLog;
 import com.masonsoft.imsdk.core.ProtoByteMessage;
 import com.masonsoft.imsdk.core.message.SessionProtoByteMessageWrapper;
 import com.masonsoft.imsdk.core.message.packet.NotNullTimeoutMessagePacket;
 import com.masonsoft.imsdk.core.proto.ProtoMessage;
+import com.masonsoft.imsdk.sample.SampleLog;
 import com.masonsoft.imsdk.sample.entity.Spark;
 import com.masonsoft.imsdk.util.Objects;
 
@@ -48,7 +48,7 @@ public class FetchSparkMessagePacket extends NotNullTimeoutMessagePacket {
                 synchronized (getStateLock()) {
                     final int state = getState();
                     if (state != STATE_WAIT_RESULT) {
-                        IMLog.e(Objects.defaultObjectTag(this)
+                        SampleLog.e(Objects.defaultObjectTag(this)
                                 + " unexpected. accept with same sign:%s and invalid state:%s", getSign(), stateToString(state));
                         return false;
                     }
@@ -56,11 +56,11 @@ public class FetchSparkMessagePacket extends NotNullTimeoutMessagePacket {
                     if (result.getCode() != 0) {
                         setErrorCode((int) result.getCode());
                         setErrorMessage(result.getMsg());
-                        IMLog.e(Objects.defaultObjectTag(this) +
+                        SampleLog.e(Objects.defaultObjectTag(this) +
                                 " unexpected. errorCode:%s, errorMessage:%s", result.getCode(), result.getMsg());
                     } else {
                         final Throwable e = new IllegalArgumentException(Objects.defaultObjectTag(this) + " unexpected. result code is 0.");
-                        IMLog.e(e);
+                        SampleLog.e(e);
                     }
                     moveToState(STATE_FAIL);
                 }
@@ -77,7 +77,7 @@ public class FetchSparkMessagePacket extends NotNullTimeoutMessagePacket {
                 synchronized (getStateLock()) {
                     final int state = getState();
                     if (state != STATE_WAIT_RESULT) {
-                        IMLog.e(Objects.defaultObjectTag(this)
+                        SampleLog.e(Objects.defaultObjectTag(this)
                                 + " unexpected. accept with same sign:%s and invalid state:%s", getSign(), stateToString(state));
                         return false;
                     }
